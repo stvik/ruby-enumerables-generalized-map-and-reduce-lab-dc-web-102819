69e494f7b50150aeff29d82dba0f58900c702d82
Sara Tarnvik <stvik2@gmail.com>
2019-10-15 18:42:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,10 +13,10 @@ end
 def reduce(source_array, starting_value = 0) 
   i = 0
   while i < source_array.length do
-     starting_value = yield(starting_value, source_array[i])
+    yield(starting_value, source_array[i])
      i += 1
   end
-  starting_value
+
 end
 
 map([1,2,3,-9]){|n| n * -1}
