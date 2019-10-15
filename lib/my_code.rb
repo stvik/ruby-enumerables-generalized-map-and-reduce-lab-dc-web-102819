d399fa9e876ddf665cfd496dabc5914d75690ea8
Sara Tarnvik <stvik2@gmail.com>
2019-10-15 18:43:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ end
 def reduce(source_array, starting_value = 0) 
   i = 0
   while i < source_array.length do
-      yield(starting_value, source_array[i])
+    starting_value = yield(starting_value, source_array[i])
      i += 1
   end
   starting_value
