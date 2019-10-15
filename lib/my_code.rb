86632ba2fc60506fc84cd4f130d2833896986f85
Sara Tarnvik <stvik2@gmail.com>
2019-10-15 18:13:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def map(source_array)
   new_array = []
   i = 0
     while i < source_array.size do
-    new_array = yield(source_array[i])
+    new_array[i] = yield(source_array[i])
     i += 1
     end
   new_array
